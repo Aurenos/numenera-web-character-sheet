310ac31dfa64d789066a2f846281c5b4ae8ba6f1
Kile Deal <kile@kjdeal.dev>
2022-08-10 17:21:15 -0400

    updated abilities control to have collapsible desc

--- src/components/SpecialAbility/SpecialAbilityControl.tsx
+++ src/components/SpecialAbility/SpecialAbilityControl.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { SpecialAbility } from "../../lib/characterSheet";
 import { Action } from "../../lib/reducer";
 
@@ -8,10 +8,11 @@ interface SpecialAbilityControlProps {
 }
 
 const SpecialAbilityControl = (props: SpecialAbilityControlProps) => {
+  const [descVisible, setDescVisible] = useState<boolean>(false);
   const { dispatch, ability } = props;
   return (
-    <div className='flex flex-col'>
-      <div className='flex flex-row mb-2'>
+    <div className='flex flex-col mb-2'>
+      <div className='flex flex-row mb-2 space-x-2'>
         <div className='tooltip tooltip-left' data-tip='Delete'>
           <button
             className='btn btn-sm btn-square btn-ghost opacity-50 rounded-none hover:opacity-100 hover:btn-error '
@@ -21,6 +22,7 @@ const SpecialAbilityControl = (props: SpecialAbilityControlProps) => {
           </button>
         </div>
         <input
+          className='text-center'
           type='text'
           placeholder='Ability Name'
           value={ability.name}
@@ -32,18 +34,31 @@ const SpecialAbilityControl = (props: SpecialAbilityControlProps) => {
             })
           }
         />
+        <button
+          className='btn btn-sm btn-square btn-ghost text-xl text-gray-600'
+          onClick={() => setDescVisible(!descVisible)}
+        >
+          {descVisible ? "^" : "v"}
+        </button>
       </div>
-      <textarea
-        placeholder='Ability Description'
-        value={ability.description}
-        onChange={evt =>
-          dispatch({
-            t: "setAbilityDescription",
-            id: ability.id,
-            description: evt.target.value,
-          })
-        }
-      />
+      <div
+        className={`collapse collapse-${descVisible ? "open" : "close"} w-full`}
+      >
+        <div className='flex collapse-content'>
+          <textarea
+            className='textarea p-1 w-full rounded-none'
+            placeholder='Ability Description'
+            value={ability.description}
+            onChange={evt =>
+              dispatch({
+                t: "setAbilityDescription",
+                id: ability.id,
+                description: evt.target.value,
+              })
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 };
