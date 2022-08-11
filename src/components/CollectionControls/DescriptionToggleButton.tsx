1d425ba87334ea0e3ff1abaaefe2afcadd213ee3
Kile Deal <kile@kjdeal.dev>
2022-08-11 02:09:35 -0400

    added icons for desc toggle

--- src/components/CollectionControls/DescriptionToggleButton.tsx
+++ src/components/CollectionControls/DescriptionToggleButton.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/outline";
 
 interface DescriptionToggleButtonProps {
   onClickHandler: React.MouseEventHandler<HTMLButtonElement>;
@@ -13,10 +14,14 @@ const DescriptionToggleButton = (props: DescriptionToggleButtonProps) => {
       data-tip={`${descVisible ? "Hide" : "Show"} Description`}
     >
       <button
-        className='btn btn-sm btn-square btn-ghost text-xl text-gray-600'
+        className='btn btn-sm btn-square btn-ghost text-gray-600'
         onClick={onClickHandler}
       >
-        {descVisible ? "^" : "v"}
+        {descVisible ? (
+          <ChevronUpIcon className='h-4 w-4' />
+        ) : (
+          <ChevronDownIcon className='h-4 w-4' />
+        )}
       </button>
     </div>
   );
